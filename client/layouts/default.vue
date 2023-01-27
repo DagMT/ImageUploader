56dfa6db3906c7a8f6b85e8fc34910dd61162b51
Dagim Damtew <78877931+DagMT@users.noreply.github.com>
2023-01-27 08:50:00 +0000

    feature:  Nuxt boilerplate setup
    - Setup a project structure and directory

---
+++ client/layouts/default.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        PyrexPics
+        <slot />
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ client/pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        DevChallenges Image Uploader challenge
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
