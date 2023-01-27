c44f83274713ce05f02e2feed60e7f845783743f
Dagim Damtew <78877931+DagMT@users.noreply.github.com>
2023-01-27 08:58:54 +0000

    feature: Nuxt color-mode configuration
    - Added a configuration module of nuxt color-mode in the nuxt config file
    - Added a component and temp styling to toggle themes on the default layout view

--- client/layouts/default.vue
+++ client/layouts/default.vue
@@ -1,6 +1,14 @@
 <template>
     <div>
         PyrexPics
+        <div>
+            <select v-model="$colorMode.preference">
+                <option value="system">System</option>
+                <option value="light">Light</option>
+                <option value="dark">Dark</option>
+                <option value="sepia">Sepia</option>
+            </select>
+        </div>
         <slot />
     </div>
 </template>
@@ -9,6 +17,19 @@
 
 </script>
 
-<style lang="scss" scoped>
+<style >
+body {
+    background-color: #fff;
+    color: rgba(0, 0, 0, 0.8);
+}
 
+.dark-mode body {
+    background-color: #091a28;
+    color: #ebf4f1;
+}
+
+.sepia-mode body {
+    background-color: #f1e7d0;
+    color: #433422;
+}
 </style>
